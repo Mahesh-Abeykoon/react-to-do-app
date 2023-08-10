ef8dae0562c53db44808b51d8078d34ad63cb20a
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-08-10 17:41:19 +0530

    Add states and pass the data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,38 @@
 
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [item, addItem] = useState([]);
+
+
+  function handleChange(event){
+    const newValue = event.target.value;
+    setInput(newValue);
+      }
+
+  function handleClick(){
+    addItem((prevItem)=>{
+      return [...prevItem, input]
+    })
+    setInput("")
+  }
+
   return (
     <div className="App">
       <div className="App-header">
         <h1>My To-do List</h1>
       </div>
-
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={input}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+         {item.map(todoItems=> <li>{todoItems}</li>)}
         </ul>
       </div>
     </div>
