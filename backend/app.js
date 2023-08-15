880f10dd43dd341069f517d1a8af2f27e1054c1d
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-08-15 19:54:05 +0530

    Add the backend

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const tasksRoutes = require('./routes/taskRoutes');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/todoDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/api/tasks', tasksRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong' });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
