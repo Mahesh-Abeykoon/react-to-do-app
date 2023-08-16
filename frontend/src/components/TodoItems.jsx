4aa0b7a8780207f6bfaf62b339074e2cca2385c0
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-08-16 23:08:39 +0530

    Add state management

--- frontend/src/components/TodoItems.jsx
+++ frontend/src/components/TodoItems.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React,{useState} from "react";
 
 function TodoItems(props) {
+   const [isEditing, setIsEditing] = useState(false);
+   const [editedText, setEditedText] = useState(props.text);
+ 
+   const handleEditClick = () => {
+     setIsEditing(true);
+   };
+ 
+   const handleEditChange = (event) => {
+     setEditedText(event.target.value);
+   };
+ 
+   const handleSaveClick = () => {
+     setIsEditing(false);
+     props.onEdit(props.id, editedText);
+   };
+
+   const handleCancelClick = () => {
+    setIsEditing(false);
+    setEditedText(props.text); 
+  };
+  
   return (
     <div>
       <li>
-        <span className="task-text">{props.text}</span>
-        <button className="delete-button"  onClick={() => props.onChecked(props.id)}>X</button>
+        <div >
+          {isEditing ? (
+                    <div className="edit-mode">
+                      <input
+                        className="edit-text"
+                        type="text"
+                        value={editedText}
+                        onChange={handleEditChange}
+                      />
+                      <div className="edit-buttons">
+                        <button className="save-button" onClick={handleSaveClick}>Save</button>
+                        <button className="cancel-button"onClick={handleCancelClick}>Cancel</button>
+                      </div>
+                      </div>
+                  ) : (
+                    <>
+                      <button className="edit" onClick={handleEditClick}>
+                        {props.text}
+                      </button>
+                    </>
+                  )}
+        </div>
+          {/* {
+            isEditing ? ( null )
+          : ( <button className="delete-button"  onClick={() => props.onChecked(props.id)}>X</button> )
+          } */}
+           {!isEditing && (
+          <button
+            className="delete-button"
+            onClick={() => {
+              if (window.confirm("Are you sure you want to delete this task?")) {
+                props.onChecked(props.id);
+            } 
+          }}> X </button>
+        )}
       </li>
     </div>
   );
