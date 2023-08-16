77174342a81393ab58e739b8a4d68a44c512913f
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-08-16 23:01:01 +0530

    Add the route

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,6 +4,7 @@ const taskController = require('../controllers/taskController');
 
 router.get('/', taskController.getAllTasks);
 router.post('/', taskController.createTask);
+router.put('/:id', taskController.editTask)
 router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
